0ee1e95c21ef6ee887946f7903f8e415a15187fb
Andrey Borodulin <remediassance@gmail.com>
2016-03-27 15:43:03 +0300

    Added buttonpress listeners

--- app/src/main/java/petrsu/smartroom/android/srcli/CityGallery.java
+++ app/src/main/java/petrsu/smartroom/android/srcli/CityGallery.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mikepenz.iconics.typeface.FontAwesome;
@@ -24,7 +27,11 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
  * Class for fruct demo.
  * Asks for city and returns its pictures
  */
-public class CityGallery extends ActionBarActivity  {
+public class CityGallery extends ActionBarActivity implements View.OnClickListener {
+
+    private Button cityBtn;
+    private EditText cityText;
+    private TextView displayedCity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,13 @@ public class CityGallery extends ActionBarActivity  {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        cityBtn = (Button) findViewById (R.id.searchBtn);
+        cityBtn.setOnClickListener(this);
+
+        cityText = (EditText) findViewById(R.id.cityTxt);
+
+        displayedCity = (TextView) findViewById(R.id.displayedText);
 
         Drawer result = new DrawerBuilder()
                 .withActivity(this)
@@ -208,5 +222,11 @@ public class CityGallery extends ActionBarActivity  {
         builder.setMessage(Html.fromHtml(getResources().getString(R.string.agenda_help_content)));
         builder.create();
         builder.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        String city = cityText.getText().toString();
+        displayedCity.setText(city);
     }
 }
