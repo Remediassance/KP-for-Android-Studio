3d939451dcab33b89675222487d508e9a1ac623a
Andrey Borodulin <remediassance@gmail.com>
2016-09-21 14:56:14 +0300

    Все. готово. Вот. Ура.

--- app/src/jni/src/kp.c
+++ app/src/jni/src/kp.c
@@ -502,20 +502,21 @@ JNIEXPORT jint JNICALL Java_petrsu_smartroom_android_srcli_KP_setCity(JNIEnv *en
 	const char *_uuid = (*env)->GetStringUTFChars(env, uuid, NULL);
 	const char *_city = (*env)->GetStringUTFChars(env, city, NULL);
 
-	individual_t* place = (individual_t *)placeExists(_city); // Связяно ли как-то время создания индивида с его переприсваиванием?
-	individual_t* person = (individual_t *)sslog_repo_get_individual_by_uuid(_uuid);
+	individual_t* newPlace;
+	individual_t* oldPlace;
+	individual_t* person;
 
-	//__android_log_print(ANDROID_LOG_INFO, "setCity():", (char*)_city);
+	prop_val_t *op_prop;
 
-	if(place == NULL){
+	newPlace = (individual_t *)placeExists(_city);
+
+	if(newPlace == NULL){
 		__android_log_print(ANDROID_LOG_INFO, "setCity():", "Place does not exist!");
-		place = createPlace(_city);
+		newPlace = createPlace(_city);
 	}
-	else {
-		__android_log_print(ANDROID_LOG_INFO, "setCity():", "Place exists.");
-	}
+	else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Place exists.");
 
-	place = sslog_repo_get_individual_by_uuid(place -> uuid); // :(
+	person = (individual_t *)sslog_repo_get_individual_by_uuid(_uuid);
 
 	if(person == NULL){
 		__android_log_print(ANDROID_LOG_ERROR, "setCity():", "Cannot get person by uuid");
@@ -523,24 +524,50 @@ JNIEXPORT jint JNICALL Java_petrsu_smartroom_android_srcli_KP_setCity(JNIEnv *en
 	}
 	else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Got person by uuid!");
 
-	if(sslog_add_property(person, PROPERTY_CITY, (void *)place) == -1){
-		__android_log_print(ANDROID_LOG_ERROR, "setCity():", "Cannot add PROPERTY_CITY to the person");
-		return -1;
-	}
-	else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Added property to the individual!");
+	op_prop = sslog_ss_get_property(person, PROPERTY_CITY);
+	oldPlace = (individual_t*)(op_prop -> prop_value);
 
+	/*
+	* Do if city wasn't set before fot this individual
+	*/
+	if(oldPlace == NULL){
+		__android_log_print(ANDROID_LOG_INFO, "setCity():", "PROPERTY_CITY was not set before!");
 
-	if(sslog_ss_insert_individual(person) == -1){
-		__android_log_print(ANDROID_LOG_ERROR, "setCity():", "Cannot insert individual");
-		return -1;
+		if(sslog_add_property(person, PROPERTY_CITY, (void *)newPlace) == -1){
+			__android_log_print(ANDROID_LOG_ERROR, "setCity():", "Cannot add PROPERTY_CITY to the person");
+			return -1;
+		} else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Added property to the individual!");
+
+		if(sslog_ss_insert_individual(person) == -1){
+			__android_log_print(ANDROID_LOG_ERROR, "setCity():", "Cannot insert individual");
+			return -1;
+		} else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Inserted the individual to the SS!");
+	} 
+	/*
+	* Do this if city was set before for this individual
+	*/
+	else {
+		__android_log_print(ANDROID_LOG_INFO, "setCity():", "PROPERTY_CITY was set before!");
+
+		/*if(sslog_ss_update_property(person, PROPERTY_CITY, (void*)oldPlace, (void*)newPlace) == -1){
+			__android_log_print(ANDROID_LOG_ERROR, "setCity():", "Error updating PROPERTY_CITY to a new value");
+			return -1;
+		} else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Updated PROPERTY_CITY successfully!");*/
+
+		sslog_ss_remove_property_all(person, PROPERTY_CITY);
+
+		sslog_ss_add_property(person, PROPERTY_CITY, (void*)newPlace);
+
+		/*if(sslog_ss_update_individual(person) == -1){
+			__android_log_print(ANDROID_LOG_ERROR, "setCity():", "Error updating the individual");
+			return -1;
+		} else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Updated individual successfully!");*/
 	}
-	else __android_log_print(ANDROID_LOG_INFO, "setCity():", "Inserted the individual to the SS!");
 
 	(*env)->ReleaseStringUTFChars(env, city, _city);
 	(*env)->ReleaseStringUTFChars(env, uuid, _uuid);
 
 	return 0;
-
 }
 
 
